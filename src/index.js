1b1b37710d0f9bf569908db3bb6ee37c60e159b0
Gabriela Faria de Oliveira <gabrielafariaoliveira@gmail.com>
2020-02-14 19:21:46 -0300

    Interface WEB desenvolvida com React JS.

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root')); // está renderizando o conteúdo contido no arquivo App que foi importado de ./App
