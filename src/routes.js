cbc8dbad192091d082e6bdd59837bd7108b9e128
Wilgnne <w.khawanbarbosa@gmail.com>
2020-02-23 15:47:08 -0300

    feat: Add React Router

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+import Simulation from './pages/simulation';
+import Welcome from './pages/welcome';
+
+function Routes() {
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route exect path="/simu/:id" component={Simulation} />
+
+        <Route exect path="/">
+          <Welcome />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
