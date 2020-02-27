b67c3945f85f72bb17cb461e3f37713e7602aa6b
Gabriela Faria de Oliveira <gabrielafariaoliveira@gmail.com>
2020-02-27 13:49:55 -0300

    feat: Nova matriz
    Adição de novas matrizes em tempo de execução

--- src/pages/simulation/index.js
+++ src/pages/simulation/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { TiArrowLeft } from "react-icons/ti";
 import { IoIosAddCircle } from "react-icons/io";
-
+import { IoMdExpand } from "react-icons/io";
+import { IoMdContract } from "react-icons/io";
 import './styles.css'
 
 //Componente: Bloco isolado de HTML, o qual não interfere no restante da aplicação. 
@@ -10,47 +11,62 @@ import './styles.css'
 
 //Toda função que um componente tem deve ser criada dentro da função do componente.
 function Simulation() {
+  const [contMatriz, setCont] = useState(['Matriz']);
+  const [openMatriz, setOpen] = useState(false);
+
+  function tratarCont() {
+    setCont([...contMatriz, "Matriz"]);
+  };
+
+  function toggleMatriz(){
+    setOpen(!openMatriz);
+  }
+
   return (
     <>
       <header className="header">
         <nav>
-          <icon className="voltar"><TiArrowLeft size={30} /></icon>
+          <div className="voltar"><TiArrowLeft size={30} /></div>
         </nav>
       </header>
 
       <section className="flexheuristica">
-        <div>
-          <p>Heurística 1</p>
-          <div className="matriz">
-            <a>MATRIZ</a>
-          </div>
-          <ul className="infos">
-            <li><a>Info 1</a></li>
-            <li><a>Info 2</a></li>
-            <li><a>Info 3</a></li>
-          </ul>
-          <div className="maisinfo">
-            <icon><IoIosAddCircle color='#fcfcfc' size={20}/></icon>
-          </div>
+
+        {
+          contMatriz.map( (matriz, index) => (
+            <div>
+        <strong>Heurística {index}</strong>
+        <div className="matriz">
+          <a>Matriz</a>
         </div>
-        <div>
-          
-          <p>Heurística 1</p>
-          <div className="matriz">
-            <a>MATRIZ</a>
-          </div>
-          <ul className="infos">
-            <li><a>Info 1</a></li>
-            <li><a>Info 2</a></li>
-            <li><a>Info 3</a></li>
-          </ul>
-          <div className="maisinfo">
-            <icon><IoIosAddCircle color='#fcfcfc' size={20}/></icon>
-          </div>
+        <ul className="infos">
+          <li><a>Info 1</a></li>
+          <li><a>Info 2</a></li>
+          <li><a>Info 3</a></li>
+          {
+            openMatriz?(
+              <ul>
+                <li><a>Info 4</a></li>
+                <li><a>Info 5</a></li>
+              </ul>  
+            ):null
+          }
+        </ul>
+        <div className="maisinfo">
+
+          {
+            openMatriz? (
+            <div onClick={(e) => toggleMatriz(e)} className="extendInfo"><IoMdContract color='#fcfcfc' size={20}/></div>
+            ):  <div onClick={toggleMatriz} className="extendInfo"><IoMdExpand color='#fcfcfc' size={20}/></div>
+          }
         </div>
+      </div>
+
+          ))
+        }
 
 
-        <button className="addButtom"><icon><IoIosAddCircle color='#fcfcfc' size={20}/></icon></button>
+        <button onClick={tratarCont} className="addButtom"><div><IoIosAddCircle color='#fcfcfc' size={20}/></div></button>
       </section>
       
       <section className="flexaplicacoes">
