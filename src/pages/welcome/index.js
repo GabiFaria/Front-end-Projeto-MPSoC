d6a076159d2958065c72811511547b2e2cb1c6be
Wilgnne <w.khawanbarbosa@gmail.com>
2020-03-01 18:42:06 -0300

    feat: página inicial v1

--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -1,14 +1,46 @@
 import React from 'react';
+import { MdMemory } from "react-icons/md";
+import { MdDeveloperBoard } from "react-icons/md";
+import { MdSubject } from "react-icons/md";
+import { MdDeviceHub } from "react-icons/md";
 
-import './styles.css'
+import './styles.css';
 
 function Welcome() {
 
-	return (
-		<>
-			<strong>Welcome</strong>
-		</>
-	);
+  return (
+    <div className="welcome">
+      <header>
+        <MdMemory size={192} color='#724EC0'  />
+        <h1>MPSoC Simulator</h1>
+      </header>
+
+      <main>
+        <ul>
+          <li>
+            <button>
+              <MdDeveloperBoard size={48} color='#5A3E90' />
+              <p>Nova Simulação</p>
+            </button>
+          </li>
+
+          <li>
+            <button>
+              <MdSubject size={48} color='#5A3E90' />
+              <p>Nova Héuristica</p>
+            </button>
+          </li>
+
+          <li>
+            <button>
+              <MdDeviceHub size={48} color='#5A3E90' />
+              <p>Nova Aplicação</p>
+            </button>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
 }
 
 export default Welcome;
