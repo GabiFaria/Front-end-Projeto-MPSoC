0d9181ed8eba82050fb64bac9ddb2d299db41f4b
Wilgnne <w.khawanbarbosa@gmail.com>
2020-03-02 09:39:50 -0300

    refactor: Removida importação não usada

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Simulation from './pages/simulation';
 import Welcome from './pages/welcome';
